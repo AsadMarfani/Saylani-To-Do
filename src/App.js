272b4b3b99d8032f82b150ce9d7585c13b5b8522
daniyal <daniyal12333@hotmail.com>
2017-03-18 12:33:50 +0500

    InitializeFirebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.addToDo = this.addToDo.bind(this);
+  }
+  
+addToDo(e){
+  e.preventDefault();
+  let refRoot  = firebase.database().ref('/todos/');
+  refRoot.push({todo: this.refs.todo.value});
+  this.refs.todo.value = '';
+  console.log(this.refs.todo.value);
+}
+
   render() {
     return (
-      <div>
-        <h1>React ToDo List</h1>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React ToDo List</h2>
+        </div>
+        <div className="formContainer">
+          <form onSubmit = {this.addToDo}>
+            <input type="text" ref = "todo" placeholder="ToDo Item"/>
+            <button>ADD TODO</button>
+          </form>
+        </div>
       </div>
     );
   }
